64b96d17402c4d2ac50f8c2a3da9fe5dad2ae840
HOGLATER <121622425+HOGLATER@users.noreply.github.com>
2023-04-20 18:04:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+let userInput = prompt('введите двузначное число');
+function toDo(userInput) {
+	if (  isNaN( userInput ) ) console.log( 'вы ввели не число');
+    else if ('userInput > 9 && prompt < 100')
+     console.log('окс');
+}
+toDo(userInput);
+function rubert(o,k){
+    alert('o + k');
+    if( 'o > k')
+    alert(o);
+    else 
+
+
+}
+
+
+
